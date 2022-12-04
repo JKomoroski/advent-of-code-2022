71c1944b94e809d84a1c9cfb9236f733f6d8c7e8
Jared Komoroski <jkomoroski@alianza.com>
2022-12-03 18:18:23 -0700

    Day 3 Solution

--- src/main/java/org/example/DayThree.java
+++ src/main/java/org/example/DayThree.java
@@ -1,11 +1,63 @@
 package org.example;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class DayThree {
 
     public static void main(String[] args) throws Exception {
         List<String> lines = StaticUtils.readFile("./input_3.txt");
 
+        final var prioritySum = lines.stream()
+                .map(s -> new String[]{s.substring(0, s.length() / 2), s.substring(s.length() / 2)})
+                .map(p -> findFirstIntersectingCharacter(p[0], p[1]))
+                .mapToInt(DayThree::charToPriority)
+                .sum();
+
+        System.out.println("Part 1 Solution: " + prioritySum);
+
+        int badgePriority = 0;
+        for (int i = 0; i < lines.size(); i += 3) {
+            int i2 = i + 1;
+            int i3 = i + 2;
+            final var chars1 = findAllIntersectingCharacters(lines.get(i), lines.get(i2));
+            final var chars2 = findAllIntersectingCharacters(lines.get(i2), lines.get(i3));
+
+            chars1.retainAll(chars2);
+
+            final var sum = chars1.stream()
+                    .mapToInt(DayThree::charToPriority)
+                    .sum();
+
+            badgePriority += sum;
+        }
+
+        System.out.println("Part 2 Solution: " + badgePriority);
+
+    }
+
+    static Set<Character> findAllIntersectingCharacters(String s1, String s2) {
+        return s2.chars()
+                .map(s1::indexOf)
+                .filter(i -> i > -1)
+                .map(s1::charAt)
+                .mapToObj(i -> (char) i)
+                .collect(Collectors.toSet());
+    }
+
+    static char findFirstIntersectingCharacter(String s1, String s2) {
+        return (char) s2.chars()
+                .map(s1::indexOf)
+                .filter(i -> i > -1)
+                .map(s1::charAt)
+                .findFirst()
+                .orElseThrow();
+    }
+
+    static int charToPriority(char character) {
+        return Character.isLowerCase(character)
+                ? (int) character - 96
+                : (int) character - 38;
     }
 }
