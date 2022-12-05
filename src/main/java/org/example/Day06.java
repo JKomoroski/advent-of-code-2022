4690045e95903fec3e562cb77be8699e3bec73c8
Jared Komoroski <jkomoroski@alianza.com>
2022-12-04 23:06:00 -0700

    Stage Day 06

---
+++ src/main/java/org/example/Day06.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Day06 {
+
+    public static void main(String[] args) throws Exception {
+        final var list = StaticUtils.readFile("./input_06.txt");
+    }
+
+}
